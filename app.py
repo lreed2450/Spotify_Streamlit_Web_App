fe623fd742fcb84e8538ba6a3eb74a1e1951a5e5
lreed2450 <74885836+lreed2450@users.noreply.github.com>
2024-09-26 12:52:05 -0500

    concat

--- app.py
+++ app.py
@@ -97,8 +97,8 @@ def top_artists(limit, time_range):
     for artist in top_artists['items']:
         artist_name = top_artists['items'][idx]['name']
         genres = top_artists['items'][idx]['genres']
-        artist_ser = artist_ser.append(artist_name)
-        genres_ser = genres_ser.append(genres)
+        artist_ser = artist_ser.concat(artist_name)
+        genres_ser = genres_ser.concat(genres)
         # artists_df = artists_df.append({'artist_name': artist_name, 'genres': genres},ignore_index=True)
         # artists_df = artists_df.append({'artist_name': artist_name, 'genres': genres}, ignore_index=True)
         idx += 1
